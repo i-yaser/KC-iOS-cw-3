fcf300f08d1fcd853b58cd31d62effd110305f59
ياسر حمد العجمي <109863263+i-yaser@users.noreply.github.com>
2022-08-05 20:03:53 +0300

    non

---
+++ test/MyPlayground.playground/Contents.swift
@@ -0,0 +1,7 @@
+import UIKit
+
+var favouriteNumber = "22"
+var name : String = "Yaser Alajmi"
+var hobby = "learn SWFT"
+
+  print("Hello, i'm \(name) my favouriteNumber is \(favouriteNumber) and i love \(hobby)")
